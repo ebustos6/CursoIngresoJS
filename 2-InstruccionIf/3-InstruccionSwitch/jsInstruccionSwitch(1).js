2685c38b1ca8f8103078e76be942f16835138dc4
ebustos6 <60674651+ebustos6@users.noreply.github.com>
2020-02-12 19:35:26 -0300

    Add files via upload

---
+++ 2-InstruccionIf/3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -0,0 +1,26 @@
+function mostrar()
+{
+
+var mes = document.getElementById('mes').value;
+
+switch(mes){
+   case "Enero":
+       alert("Que comiences bien el año!!");
+       break;
+
+    case "Marzo":
+        alert("A clases!!");
+        break;
+
+    case "Julio":
+        alert("Se vienen las vacaciones!!");
+        break;
+
+    case "Diciembre":
+        alert("Felices fiestas!!")
+        break;
+}
+
+
+
+}//FIN DE LA FUNCIÓN
