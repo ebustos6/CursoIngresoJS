1701888900bd4fe6fda6b0b0ba28dbc193c27629
ebustos6 <60674651+ebustos6@users.noreply.github.com>
2020-02-11 22:19:27 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,12 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+	var num1;
+    var num2;
+
+    num1 = document.getElementById("elNombre").value;
+    num2 = document.getElementById("laEdad").value;
+
+    alert(" " + num1 + " Y su edad es " + num2 + ".")
 }
 
