a9f2446c02d10d3d17480b42424b1baaf9839419
ebustos6 <60674651+ebustos6@users.noreply.github.com>
2020-02-12 22:17:15 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,11 @@
 function mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
-
-
+var clave = "utn750";
+clave = prompt("Ingrese el número clave.");
+while(clave!="utn750"){
+    alert("La clave es incorrecta")
+clave = prompt("Ingrese nuevamente la clave")
+}
+alert("Clave correcta")
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,7 +1,12 @@
 function mostrar()
 {
 
-	var numero = prompt("ingrese un número entre 0 y 10.");
+	var numero;
+	numero = parseInt(prompt("Ingrese un número entre 0 y 9."));
+	while (numero>9 || numero<0 || isNaN(numero)){
+		
+		numero = parseInt(prompt("Numero invalido. Ingrese un número entre 0 y 9."));
 
-
+	}
+	document.getElementById("Numero").value = numero;
 }//FIN DE LA FUNCIÓN
