298f8a074410a5109cb7020c97cf690d721ada03
ebustos6 <60674651+ebustos6@users.noreply.github.com>
2020-02-12 22:17:55 -0300

    Add files via upload

---
+++ jsInstruccionSwitch(1).js
@@ -0,0 +1,28 @@
+function mostrar()
+{
+
+var mes = document.getElementById('mes').value;
+
+switch(mes){
+   case "Enero":
+       alert("Que comiences bien el año!!");
+       break;
+
+    case "Marzo":
+        alert("A clases!!");
+        break;
+
+    case "Julio":
+        alert("Se vienen las vacaciones!!");
+        break;
+
+    case "Diciembre":
+        alert("Felices fiestas!!")
+        break;
+    /* default:
+      alert("A ese mes no lo tuve en cuenta.");*/  
+}
+
+
+
+}//FIN DE LA FUNCIÓN
