cc2dab9e28cfbfa9d00261e6cde132cd13607b6c
ebustos6 <60674651+ebustos6@users.noreply.github.com>
2020-02-19 22:17:21 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -16,9 +16,16 @@ function mostrar() {
 
 		respuesta = prompt("Quiere ingresar otro número?");
 
-
-
-	} while (respuesta == "s")
-
+	} while (respuesta == "s");
+/*	1-Suma de los negativos.| if, elseif
+	2-Suma de los positivos.|
+	3-Cantidad de positivos. |
+	4-Cantidad de negativos. |
+	5-Cantidad de ceros. |
+	6-Cantidad de números pares. | if aparte
+	7-Promedio de positivos.|
+	8-Promedios de negativos.|
+	9-Diferencia entre positivos y negativos, (positvos-negativos).|
+	*/
 }
 	//FIN DE LA FUNCIÓN
