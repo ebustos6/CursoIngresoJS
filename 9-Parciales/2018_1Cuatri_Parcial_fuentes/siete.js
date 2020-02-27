e41a07193069e15bba19d231c3c029a53cf23f55
ebustos6 <60674651+ebustos6@users.noreply.github.com>
2020-02-26 22:22:39 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -2,16 +2,17 @@ function mostrar()
 {
 var nota;
 var sexo;
-var i = 0;
+var i;
 var acumulador = 0;
 var minimo;
+var min_sex;
 var flag = 0;
 var varones_ap = 0;
 
-do{
+for(i = 0;i < 5;i++){
     nota = parseInt(prompt("Ingrese la nota:"));
 
-    while(isNaN(nota)){
+    while(isNaN(nota)||nota< 0||nota > 10){
         nota = parseInt(prompt("Ingrese un número valido:"));
       }
 
@@ -21,10 +22,10 @@ do{
         sexo = prompt("Dato incorrecto, ingrese nuevamente el sexo del alumno.");
     }
     acumulador = acumulador + nota;
-    i++;
    
     if(flag == 0 || nota < minimo){
         minimo = nota;
+        min_sex = sexo;
         flag = 1;
     }
 
@@ -33,11 +34,9 @@ do{
     }
     
 
-} while(i < 5);
+} 
 
-alert("El promedio de las notas es: " + (acumulador / 5)) = acumulador;
-alert("La nota mas baja es " + minimo + " y su sexo es " + sexo) = minimo;
-alert("Cantidad de varones aprobados: " + varones_ap) = varones_ap;
+alert("El promedio de las notas es: " + (acumulador / 5) + " ." + "La nota mas baja es " + minimo + " y su sexo es " + min_sex + " ."+ "Cantidad de varones aprobados: " + varones_ap) = acumulador;
 
 }
 
